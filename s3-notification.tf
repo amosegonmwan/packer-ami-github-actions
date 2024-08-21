3cb9659f337f175417ff69a37e9230062ebadf37
Amos Egonmwan <amos.egonmwan@hotmail.com>
2024-08-21 16:11:27 -0600

    Setup CI pipeline for building and uploading AMIs using Packer

--- s3-notification.tf
+++ s3-notification.tf
@@ -10,7 +10,7 @@ terraform {
 
   backend "s3" {
     bucket = "s3-remote-backend-2024"
-    key    = "github-actions/terraform.tfstate"
+    key    = "packer-ami-actions/terraform.tfstate"
     region = "us-west-2"
   }
 }
@@ -33,7 +33,7 @@ resource "aws_sns_topic_subscription" "email_subscription" {
 
 # Configure S3 Bucket Notification:
 resource "aws_s3_bucket_notification" "bucket_notification" {
-  bucket = "s3-remote-backend-2024"
+  bucket = "dev-artifacts-repo"
 
   topic {
     topic_arn = aws_sns_topic.s3_notification_topic.arn
