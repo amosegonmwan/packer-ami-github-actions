b07c512af05b60bbcd2efb2ec4c3df14c854e747
Amos Egonmwan <amos.egonmwan@hotmail.com>
2024-08-21 15:58:26 -0600

    Setup CI pipeline for building and uploading AMIs using Packer

--- s3-notification.tf
+++ s3-notification.tf
@@ -1,10 +1,17 @@
 terraform {
   required_version = "~> 1.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+  }
+
+  backend "s3" {
+    bucket = "s3-remote-backend-2024"
+    key    = "github-actions/terraform.tfstate"
+    region = "us-west-2"
   }
 }
 
