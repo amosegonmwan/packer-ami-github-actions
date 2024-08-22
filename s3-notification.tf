1b37694320442869f30b8284151b3e52e71e4258
Amos Egonmwan <amos.egonmwan@hotmail.com>
2024-08-21 22:02:55 -0600

    6

--- s3-notification.tf
+++ s3-notification.tf
@@ -15,59 +15,57 @@ terraform {
   }
 }
 
+# Define the AWS provider
 provider "aws" {
   region = "us-west-2"
 }
-
-# Data source to reference the existing S3 bucket:
-data "aws_s3_bucket" "existing_bucket" {
+#########################################################
+# Reference to the existing S3 bucket using a data source
+data "aws_s3_bucket" "dev_artifacts_repo" {
   bucket = "dev-artifacts-repo"
 }
 
-# Create the SNS Topic without a policy
-resource "aws_sns_topic" "s3_notification_topic" {
-  name = "packer-aws-ami-with-git-actions"
+# Create an SNS topic
+resource "aws_sns_topic" "s3_bucket_notifications" {
+  name = "s3-bucket-email-notifications"
 }
 
-# Create the SNS Topic Policy separately
-resource "aws_sns_topic_policy" "s3_notification_topic_policy" {
-  arn = aws_sns_topic.s3_notification_topic.arn
-
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": "*"
-      },
-      "Action": "SNS:Publish",
-      "Resource": "${aws_sns_topic.s3_notification_topic.arn}",
-      "Condition": {
-        "ArnLike": {
-          "aws:SourceArn": "${data.aws_s3_bucket.existing_bucket.arn}"
+# Create a policy for the SNS topic allowing S3 to publish to it
+resource "aws_sns_topic_policy" "sns_topic_policy" {
+  arn    = aws_sns_topic.s3_bucket_notifications.arn
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "s3.amazonaws.com"
+        }
+        Action = "SNS:Publish"
+        Resource = aws_sns_topic.s3_bucket_notifications.arn
+        Condition = {
+          ArnLike = {
+            "aws:SourceArn" = "arn:aws:s3:::${data.aws_s3_bucket.dev_artifacts_repo.bucket}"
+          }
         }
       }
-    }
-  ]
-}
-POLICY
+    ]
+  })
 }
 
-# Subscribe an Email to the SNS Topic:
+# Subscribe your email address to the SNS topic
 resource "aws_sns_topic_subscription" "email_subscription" {
-  topic_arn = aws_sns_topic.s3_notification_topic.arn
+  topic_arn = aws_sns_topic.s3_bucket_notifications.arn
   protocol  = "email"
   endpoint  = "amos.egonmwan@hotmail.com"
 }
 
-# Configure S3 Bucket Notification:
-resource "aws_s3_bucket_notification" "bucket_notification" {
-  bucket = data.aws_s3_bucket.existing_bucket.id
+# Create S3 bucket notification to trigger SNS topic
+resource "aws_s3_bucket_notification" "s3_event_notifications" {
+  bucket = data.aws_s3_bucket.dev_artifacts_repo.id
 
   topic {
-    topic_arn = aws_sns_topic.s3_notification_topic.arn
-    events    = ["s3:ObjectCreated:*"]
+    topic_arn = aws_sns_topic.s3_bucket_notifications.arn
+    events    = ["s3:ObjectCreated:*", "s3:ObjectRemoved:*", "s3:ObjectRestore:Completed", "s3:Replication:*"]
   }
 }
